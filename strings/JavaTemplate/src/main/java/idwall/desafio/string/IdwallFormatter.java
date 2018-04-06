83679a3874737b00826d04feedd1f3e7fc5c1396
Fabricio Machado - Vendor <Fabricio.Machado@walmart.com>
2018-04-06 14:54:30 -0300

    created string challenge

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -5,14 +5,112 @@ package idwall.desafio.string;
  */
 public class IdwallFormatter extends StringFormatter {
 
-    /**
-     * Should format as described in the challenge
-     *
-     * @param text
-     * @return
-     */
-    @Override
-    public String format(String text) {
-        throw new UnsupportedOperationException();
-    }
+	public IdwallFormatter(int limit) {
+		super(limit);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Should format as described in the challenge
+	 *
+	 * @param text
+	 * @param limit
+	 * @param justify
+	 * @return textLine
+	 * @throws Exception
+	 */
+	@Override
+	public String format(String text, int limit, boolean justify) throws Exception {
+
+		String line = "";
+		String textLine = "";
+
+		if (text.length() == 0) {
+			return textLine = "Text empty";
+		}
+
+		try {
+			String[] splitText = text.split(" ");
+
+			if ((splitText.length == 1) && (splitText[0].length() < limit)) {
+				return textLine = text;
+			}
+
+			for (int i = 0; i < splitText.length; i++) {
+
+				String word = splitText[i];
+
+				if (word.length() > limit) {
+					return textLine = "Word greater than the maximum line length.";
+				}
+
+				if (line.length() + word.length() + 1 > limit) {
+
+					if ((justify) && (line.length() < limit)) {
+						line = justifyText(line, limit);
+					}
+
+					if (textLine == "") {
+						textLine = line;
+					} else {
+						textLine = textLine + "\n" + line;
+					}
+
+					line = word;
+				} else if (line.length() == 0) {
+					line = word;
+				} else if (i + 1 == splitText.length) {
+					line = line + " " + word;
+
+					if ((justify) && (line.length() < limit)) {
+						line = justifyText(line, limit);
+					}
+
+					textLine = textLine + "\n" + line;
+				} else {
+					line = line + " " + word;
+				}
+			}
+		} catch (Exception e) {
+			throw new Exception("Error limiting text lines");
+		}
+
+		return textLine;
+	}
+
+	/**
+	 * Justify text as described in the challenge
+	 *
+	 * @param line
+	 * @param limit
+	 * @return justifyLine
+	 * @throws Exception
+	 */
+	public String justifyText(String line, int limit) throws Exception {
+
+		String justifyLine = "";
+
+		try {
+			String[] word = line.split(" ");
+			int spaces = limit - line.length() + (word.length - 1);
+
+			for (int i = 0; i < word.length - 1 && spaces > 0; i++) {
+				word[i] = word[i].concat(" ");
+
+				if (i + 1 == word.length - 1) {
+					i = -1;
+				}
+
+				spaces--;
+			}
+
+			for (int j = 0; j < word.length; j++) {
+				justifyLine = justifyLine + word[j];
+			}
+		} catch (Exception e) {
+			throw new Exception("Error justifying text");
+		}
+
+		return justifyLine;
+	}
 }
--- strings/JavaTemplate/src/main/java/idwall/desafio/string/StringFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/StringFormatter.java
@@ -5,17 +5,30 @@ package idwall.desafio.string;
  */
 public abstract class StringFormatter {
 
-    private Integer limit;
+	private Integer limit;
 
-    public StringFormatter() {
-        this.limit = 40;
-    }
+	public StringFormatter(int limit) {
+		this.setLimit(limit);
+	}
 
-    /**
-     * It receives a text and should format this text
-     *
-     * @param text
-     * @return
-     */
-    public abstract String format(String text);
+
+	/**
+	 * It receives a text and should format this text
+	 *
+	 * @param text
+	 * @param limit
+	 * @param justify
+	 * @return textLine
+	 * @throws Exception
+	 */
+	public abstract String format(String text, int limit, boolean justify) throws Exception;
+
+	public Integer getLimit() {
+		return limit;
+	}
+
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+
 }
